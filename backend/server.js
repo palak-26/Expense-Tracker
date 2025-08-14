7d1aa2c90f52b06f99e232562a64ffe9c5fc650a
Palak Neekhra <palakneekhra1234@gmail.com>
2025-08-14 23:01:51 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express(); // Initialize Express app
 // Enable CORS to allow frontend requests
 app.use(cors({
   origin: process.env.CLIENT_URL  , // Only allow this frontend URL  
-  credentials: "include", // Allow cookies & authentication headers
+  credentials: true, // Allow cookies & authentication headers
 }));
 
 // Parse incoming JSON payloads
