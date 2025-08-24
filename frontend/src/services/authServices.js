7476ae82851d15db2a12e5c1dbe79f35724e488c
Palak Neekhra <palakneekhra1234@gmail.com>
2025-08-24 12:17:28 +0530

    Added Working of ContactUs form

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -4,7 +4,7 @@ import API from "./api";
  * @param {{name?: string, email: string, password: string}} payload
  */
 export const registerUser = async (payload) => {
-  const { data } = await API.post("/register", payload);
+  const { data } = await API.post("/auth/register", payload);
   return data;
 };
 
@@ -12,7 +12,7 @@ export const registerUser = async (payload) => {
  * @param {{email: string, password: string}} payload
  */
 export const loginUser = async (payload) => {
-  const { data } = await API.post("/login", payload);
+  const { data } = await API.post("/auth/login", payload);
   const { token } = data; // destructure from response
   localStorage.setItem("token", token);
   return data;
